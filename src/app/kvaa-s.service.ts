a2d09fcdc0424a2d8950186bff05937f00230a64
schenzio <matteo.scanna1999@gmail.com>
2021-05-26 15:35:02 +0200

    1 file modified

--- src/app/kvaa-s.service.ts
+++ src/app/kvaa-s.service.ts
@@ -6,7 +6,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class KVaaSService {
-  apiURL: string = "https://api.keyvalue.xyz//myKey";
+  apiURL: string =
+  'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
+  //apiURL: string = "https://api.keyvalue.xyz//myKey";
   //apiURL: string = "https://api.keyvalue.xyz/5658665f/myKey";
   //apiURL: string = "https://api.keyvalue.xyz/5d26258c/myKey";
   constructor(private http: HttpClient) { }
@@ -18,12 +20,18 @@ export class KVaaSService {
   public postData(obJSON: Object): Observable<Object> {
     return this.http.post(this.apiURL, obJSON);
   }
-  public getKey(): Observable<Object> {
-    this.apiURL= this.apiURL.slice(0, 25)+'new'+this.apiURL.slice(25);
-    return this.http.get(this.apiURL);
-  }
   public Key() {
-    return this.http.post('https://api.keyvalue.xyz/new/myKey', '', {
-      responseType: 'text'});
-  }
+  fetch(this.apiURL + '/new', { method: 'POST' })
+    .then(response => response.json(), error => alert(error))
+    .then(key => {
+      console.log(key);
+      fetch(this.apiURL + '/post?key=' + key + '&msg=' + {}, {
+        method: 'POST'
+      })
+        .then(response => response.json(), error => alert(error))
+        .then(data => {
+          console.log(data);
+        });
+    });
+}
 }
